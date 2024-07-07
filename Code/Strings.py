c497b4331625626b29b8773cdb6dcd5627840c2c
Kshitij Darwhekar <kshitijdarwhekar@gmail.com>
2024-07-07 21:26:01 +0530

    Strings

---
+++ Code/Strings.py
@@ -0,0 +1,26 @@
+
+my_string = "Hello World"
+
+another_with_quotes = 'He Said " You were Amazing yesterday" '
+
+#Escaping using a backslash
+
+multiline = """Hello World!
+
+My Name is Joe Mama
+"""
+
+print(multiline)
+
+"""
+Strings
+
+Multiline Strings
+
+"""
+
+
+name = "Jose"
+gretting = "Hello "
+
+print(gretting+name)
