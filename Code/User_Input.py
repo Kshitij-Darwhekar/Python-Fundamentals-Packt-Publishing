54716b31b70db333a49c1408f03f56a422fb39ee
Kshitij Darwhekar <kshitijdarwhekar@gmail.com>
2024-07-07 21:44:00 +0530

    User Input

---
+++ Code/User_Input.py
@@ -0,0 +1,17 @@
+my_name = "Jose"
+
+your_name = input("Enter your name: ")
+
+print(f"Hello {your_name}. My Name is {my_name}")
+
+# Everything you enter in a input field is a string. If you want a number you wull have to convert it
+
+
+age = int(input('Enter your age: '))
+
+
+print(f"Your age is {age}")
+
+
+# Things insdie the bracker always runs first. 
+# The input function will run first and then the int conversion
