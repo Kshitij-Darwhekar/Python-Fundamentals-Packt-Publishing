bdc4331aa26fae7bdce597f5ce044a6d52968de6
Kshitij Darwhekar <kshitijdarwhekar@gmail.com>
2024-07-07 21:52:11 +0530

    Booleans

---
+++ Code/Introduction to Python/Booleans.py
@@ -0,0 +1,20 @@
+age = 20
+
+is_over_age = age >= 18
+
+is_under_age= age < 18
+
+
+#  == is a comparison operator and not a assignment operator.
+
+is_twenty = age == 20 
+
+
+number = 5
+
+user_number = int(input("Enter a number: "))
+
+matches = user_number == number
+
+print(f"You got it right: {matches}.")
+
