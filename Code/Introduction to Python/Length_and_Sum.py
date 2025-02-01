4e79432b58ad106a0706668e563e872ee09a449a
Kshitij Darwhekar <kshitijdarwhekar@gmail.com>
2025-02-01 23:49:43 +0530

    Add Length_and_Sum.py: Demonstrate length and sum functions in Python with example

---
+++ Code/Introduction to Python/Length_and_Sum.py
@@ -0,0 +1,11 @@
+# Length and SUm Function in Python
+# len() --> Returns the length of an object
+# sum() --> Sums the items of an iterator
+
+grades = [80, 75, 90, 100]
+
+total = sum(grades)
+length = len(grades)
+
+average = total / length
+print("Average: ",average)
