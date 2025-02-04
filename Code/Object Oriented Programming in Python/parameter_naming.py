775740e3a762ad1e2259c1e304b6619ef2d10b33
Kshitij Darwhekar <kshitijdarwhekar@gmail.com>
2025-02-04 15:53:05 +0530

    Add parameter_naming.py: Implement Movie class with constructor and explanatory comments

---
+++ Code/Object Oriented Programming in Python/parameter_naming.py
@@ -0,0 +1,15 @@
+# Parameter Naming in Python
+
+
+class Movie:
+    def __init__(self, name, year):
+        self.name = name 
+        self.year = year
+    
+    # dont get confused seeing self.name = name
+    # self is a blank object and by 
+    # self.name we are creating a new variable called name 
+    # in self object and giving them the value of name
+    
+        
+    
