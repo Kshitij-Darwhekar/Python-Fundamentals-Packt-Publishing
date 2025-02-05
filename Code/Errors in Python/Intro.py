1e3ad4655fd9faa9f2e38686128c09e14a823b5c
Kshitij Darwhekar <kshitijdarwhekar@gmail.com>
2025-02-05 14:45:55 +0530

    Add Intro.py: Implement introductory examples for handling errors in Python

---
+++ Code/Errors in Python/Intro.py
@@ -0,0 +1,12 @@
+# Errors in Python
+# How we can use them, how we can handle them
+
+# print(my_variable)
+
+# my_variable is not defined so we get an error message
+# We will get a NameError
+
+my_variable = 10
+
+print(my_variable)
+
